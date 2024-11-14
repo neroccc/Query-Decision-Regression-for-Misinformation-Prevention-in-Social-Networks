e58fa534b970d42088c21000b3d90e8c5748afaf
EricW1996 <76964673+EricW1996@users.noreply.github.com>
2024-11-14 15:25:22 -0500

    update

--- MetaLearner/auto.py
+++ MetaLearner/auto.py
@@ -1,9 +1,65 @@
 import os
-for i in range(1):
+for i in range(5):
 	print(i)
-	str =('python3 Soup_train.py --featureNum 160 --trainNum 108')
+	str =('python3 Soup_train.py --featureNum 10 --trainNum 27')
 	p = os.system(str)
-for i in range(1):
+for i in range(5):
 	print(i)
-	str =('python3 Soup_train.py --featureNum 160 --trainNum 216')
+	str =('python3 Soup_train.py --featureNum 10 --trainNum 54 --thread 54')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 10 --trainNum 108 --thread 108')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 10 --trainNum 216 --thread 108')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 40 --trainNum 27')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 40 --trainNum 54 --thread 54')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 40 --trainNum 108 --thread 108')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 40 --trainNum 216 --thread 108')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 80 --trainNum 27')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 80 --trainNum 54 --thread 54')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 80 --trainNum 108 --thread 108')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 80 --trainNum 216 --thread 108')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 160 --trainNum 27')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 160 --trainNum 54 --thread 54')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 160 --trainNum 108 --thread 108')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 Soup_train.py --featureNum 160 --trainNum 216 --thread 108')
 	p = os.system(str)
---
+++ MetaLearner/sauto.py.py
@@ -0,0 +1,129 @@
+import os
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 10 --trainNum 27')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 10 --trainNum 54')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 10 --trainNum 108')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 10 --trainNum 216')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 40 --trainNum 27')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 40 --trainNum 54')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 40 --trainNum 108')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 40 --trainNum 216')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 80 --trainNum 27')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 80 --trainNum 54')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 80 --trainNum 108')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 80 --trainNum 216')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 160 --trainNum 27')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 160 --trainNum 54')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 160 --trainNum 108')
+	p = os.system(str)
+for i in range(10):
+	print(i)
+	str =('python3 train.py --featureNum 160 --trainNum 216')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 100 --trainNum 270')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 100 --trainNum 540')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 100 --trainNum 1080')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 100 --trainNum 2160')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 400 --trainNum 270')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 400 --trainNum 540')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 400 --trainNum 1080')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 400 --trainNum 2160')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 800 --trainNum 270')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 800 --trainNum 540')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 800 --trainNum 1080')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 800 --trainNum 2160')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 1600 --trainNum 270')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 1600 --trainNum 540')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 1600 --trainNum 1080')
+	p = os.system(str)
+for i in range(5):
+	print(i)
+	str =('python3 train.py --featureNum 1600 --trainNum 2160')
+	p = os.system(str)
